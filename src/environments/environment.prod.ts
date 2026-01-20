51469afafe9b287ae9d1dec73119563d0d1a9f92
Qadeer Ashraf <you@example.com>
2026-01-20 18:40:12 +0100

    updates for build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  supabaseUrl: '',
-  supabaseAnonKey: ''
+  supabaseUrl: 'https://zlzgymofadtbykuimvkz.supabase.co',
+  supabaseAnonKey: 'sb_publishable_s9g83AsyXV5GhxkbVB365w_p9d0KSYO'
 };
